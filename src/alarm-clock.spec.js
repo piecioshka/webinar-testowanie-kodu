ecd1e62f783fa42c51629f72718763ffbc11456d
Piotr Kowalski <piecioshka@gmail.com>
2018-07-31 19:30:06 +0200

    Cleanup

--- src/alarm-clock.spec.js
+++ src/alarm-clock.spec.js
@@ -68,6 +68,27 @@ describe('General', () => {
             c.setAlarm(8);
             c.start();
         });
+    });
+});
 
+xdescribe('Code Coverage', () => {
+    it('should calls each of method to make 100% CC', () => {
+        global.Date.prototype.getHours = () => undefined;
+        const instance = new AlarmClock(() => undefined);
+        const methodNames = Object.getOwnPropertyNames(AlarmClock.prototype);
+        methodNames.forEach((name) => {
+            try {
+                // Funkcję typu constructor musimy uruchomić
+                // operatorem `new`
+                if (name === 'constructor') {
+                    return;
+                }
+
+                // Uruchomienie każdej metody pojedynczo
+                instance[name]();
+            } catch (err) {
+                console.log(err);
+            }
+        });
     });
 });
